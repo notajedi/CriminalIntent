e6e687ad89426c6485cc22831af981161134e636
Ivan <kyselovivan@gmail.com>
2017-04-26 15:49:31 +0300

    Crimelab modofied

--- app/src/main/java/com/example/ivan/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/ivan/criminalintent/CrimeLab.java
@@ -2,12 +2,17 @@ package com.example.ivan.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by ivan on 26.04.17.
  */
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+    List<Crime> mCrimes;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null) {
@@ -16,5 +21,26 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
-    private CrimeLab(Context context){}
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
+        for(int i=0;i<100;i++){
+            Crime crime = new Crime();
+            crime.setTitle("Crime #"+i);
+            crime.setSolved(i%2==0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes(){
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for(Crime crime:mCrimes){
+            if(crime.getId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
 }
